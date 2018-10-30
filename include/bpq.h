f6c4fe98fba348c3dfcd8c601e47a70f2d0cfe5f
rahul <anand.rahul04@gmail.com>
2018-10-30 12:14:50 +0530

    modified bpq

--- include/bpq.h
+++ include/bpq.h
@@ -7,111 +7,56 @@
 template <typename T>
 class BoundedPriorityQueue {
 private:
-    std::vector<std::pair<T, float>> queue;
-    int bound;
-    int size;
-    int maxIndex;
-    BoundedPriorityQueue();
-    void siftUp(int);
-    void siftDown(int);
-    int parent(int);
-    int left(int);
-    int right(int);
-    void swap(int, int);
+    std::vector< std::pair<T, float> > queue;
+    unsigned int bound;
+    unsigned int size;
+    void swap(unsigned int, unsigned int);
 public:
-    BoundedPriorityQueue(int);
+    BoundedPriorityQueue(unsigned int);
     void enqueue(T, float);
     T extractMin();
-    float minPriority();
+    unsigned getMaxPriority();
     bool full();
 };
 
-template <typename T>
-BoundedPriorityQueue<T>::BoundedPriorityQueue(int bound)
-    : bound(bound),
-    size(0),
-    maxIndex(0) {
-    ;
+BoundedPriorityQueue<T>::BoundedPriorityQueue(unsigned int bound)
+    :bound(bound),
+    size(0) {
+
 }
 
-template <typename T>
-void BoundedPriorityQueue<T>::swap(int index1, int index2) {
-    std::pair<T, float> temp(queue[index1]);
+void BoundedPriorityQueue<T>::swap(unsigned int index1, unsigned int index2) {
+    Point<T>* swap = queue[index1];
     queue[index1] = queue[index2];
-    queue[index2] = temp;
+    queue[index2]  = swap;
 }
 
-template <typename T>
-int BoundedPriorityQueue<T>::parent(int index) {
-    return index / 2;
-}
-
-template <typename T>
-int BoundedPriorityQueue<T>::left(int index) {
-    return 2 * index + 1;
-}
-
-template <typename T>
-int BoundedPriorityQueue<T>::right(int index) {
-    return 2 * index + 2;
-}
-
-template <typename T>
-void BoundedPriorityQueue<T>::siftUp(int index) {
-    if(index >=0 && queue[index].second < queue[parent(index)].second)  {
-        swap(index, parent(index));
-        siftUp(parent(index));
-    }
-}
-
-template <typename T>
-void BoundedPriorityQueue<T>::siftDown(int index) {
-    int l = left(index);
-    int r = right(index);
-    int smallest = index;
-    if(l <= size && queue[index].second > queue[l].second) {
-        smallest = l;
-    }
-    if(r <= size && queue[smallest].second > queue[r].second) {
-        smallest = r;
-    }
-    if(smallest != index) {
-        swap(index, smallest);
-        siftDown(smallest);
-    }
-}
-
-template <typename T>
 void BoundedPriorityQueue<T>::enqueue(T element, float priority) {
-    if(size > bound) {
-        swap(maxIndex, bound);
+    if(!full()) {
+        for(unsigned int i = 0; i < size; i++) {
+            if(priority < queue[i]) {
+                for(int j = i; j < size; j++) {
+                    queue[j+1] = queue[j];
+                }
+                queue[i] = element;
+            }
+        }
     }
-//    queue[size] = std::make_pair(element, priority);
-    std::pair<T, float> temp(element, priority);
-    queue[size] = temp;
-    siftUp(size);
-    if(priority > queue[maxIndex].second) {
-        maxIndex = size;
-    }
-    size++;
 }
 
-template <typename T>
 T BoundedPriorityQueue<T>::extractMin() {
-    std::pair<T, float> min = queue[0];
-    queue[0] = queue[size-1];
-    siftDown(0);
-    return min.first;
+    return queue[0];
 }
 
-template <typename T>
-float BoundedPriorityQueue<T>::minPriority() {
-    return queue[0].second;
+float BoundedPriorityQueue<T>::getMaxPriority() {
+    return queue[size];
 }
 
-template <typename T>
 bool BoundedPriorityQueue<T>::full() {
-    return (size == queue.size())? true: false;
+    if(size == bound) {
+        return true;
+    }
+    return false;
 }
 
 #endif
